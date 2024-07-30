4c35d8a7e8a349f5b513a71ea25430bf1f028e50
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-07-30 09:46:18 +0200

    Created styled Button as new component

--- src/features/tasks/TasksPage/Buttons/styled.js
+++ src/features/tasks/TasksPage/Buttons/styled.js
@@ -4,21 +4,3 @@ export const Wrapper = styled.div`
     display: flex;
     flex-wrap: wrap;   
 `;
-
-export const Button = styled.button`
-    border: none;
-    color: ${({ theme }) => theme.color.teal};
-    background: transparent;
-    margin: 0 0 0 20px; 
-    transition: color 0.5s;
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-        flex-basis:100px;
-        margin: 10px;
-    }
-    &:hover {
-        filter: brightness(110%);
-    }
-    &:disabled {
-        color: ${({theme}) => theme.color.lightGrey}
-    }
-`;
