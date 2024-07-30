278e33e7911d68c7c63c98fba03544cb12b9df11
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-07-30 10:25:15 +0200

    Get query from URLSearchParams and set as value in input

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Input from "../Input";
 import { Wrapper } from "./styled";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 
 export default () => {
-  
+    const location = useLocation();
+    const query = (new URLSearchParams(location.search)).get("szukaj");
+
 
     return (
         <Wrapper>
             <Input
                 placeholder="Filtruj zadania"
-                
+                value={query || ""}
             />
         </Wrapper>
     )
