6e91702dd1b82dcc3d458e738799a365b2196a97
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-07-30 09:36:17 +0200

    Created TaskPage

---
+++ src/features/tasks/TaskPage/index.js
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useSelector } from "react-redux";
+import Header from "../../../common/Header";
+import Section from "../../../common/Section";
+import { getTaskById } from "../tasksSlice";
+import Container from "../../../common/Container";
+
+function TaskPage() {
+    const { id } = useParams();
+    const task = useSelector(state => getTaskById(state, id));
+
+
+    return (
+        <Container>
+            <p>{id}</p>
+            <Header title="Szczegóły zadania" />
+            <Section
+                title={task ? task.content : "Nie znaleziono zadania 😢"}
+                body={!!task && (
+                    <>
+                        <strong>Ukończono:</strong> {task && task.done ? "Tak" : "Nie"}
+                    </>
+                )}
+            />
+        </Container>
+    );
+};
+
+export default TaskPage;
