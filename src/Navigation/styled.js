2267bfc3c647f8dcd52c15a5872251c924b13016
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-07-30 10:02:37 +0200

    Created StyledNavLink and StyledNavList

---
+++ src/Navigation/styled.js
@@ -0,0 +1,30 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const activeClassName = "active";
+
+export const StyledNavLink = styled(NavLink).attrs(() => ({
+    activeClassName,
+}))`
+    &.${activeClassName} {
+        font-weight: bold;
+        };
+
+    & {
+    text-decoration: none;
+    color: ${({ theme }) => theme.color.white};
+    margin-left: 30px;
+    };
+        &:hover{
+            text-decoration: underline;
+        };
+`;
+
+export const StyledNavList = styled.ul`
+    list-style-type: none;
+    background-color: ${({ theme }) => theme.color.teal};
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+    margin-top: 0;
+`;
