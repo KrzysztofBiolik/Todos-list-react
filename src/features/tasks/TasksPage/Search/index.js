c9963ac27e4690e503cdca634d5c8206914f5bd8
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-07-30 10:30:23 +0200

    Created function OnInputChange to set new path depended on Search input value

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Input from "../Input";
 import { Wrapper } from "./styled";
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useLocation } from "react-router-dom/cjs/react-router-dom.min";
 
 export default () => {
     const location = useLocation();
+    const history = useHistory();
     const query = (new URLSearchParams(location.search)).get("szukaj");
+
+
+    const onInputChange = ({ target }) => {
+        const searchParams = new URLSearchParams(location.search);
+
+        if (target !== undefined) {
+            searchParams.set("szukaj", target.value);
+        } else {
+            searchParams.delete("szukaj");
+        }
+
+        history.push(`${location.pathname}?${searchParams.toString()}`);
+    };
 
 
     return (
@@ -13,6 +27,7 @@ export default () => {
             <Input
                 placeholder="Filtruj zadania"
                 value={query || ""}
+                onChange={onInputChange}
             />
         </Wrapper>
     )
