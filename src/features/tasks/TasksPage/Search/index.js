c443af61de3ac03743c72a31c1d8336123f8a293
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-07-30 10:37:30 +0200

    Created and used searchQueryParamName

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -2,20 +2,22 @@ import React from "react";
 import Input from "../Input";
 import { Wrapper } from "./styled";
 import { useHistory, useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import searchQueryParamName from "../searchQueryParamName";
+
 
 export default () => {
     const location = useLocation();
     const history = useHistory();
-    const query = (new URLSearchParams(location.search)).get("szukaj");
+    const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
 
 
     const onInputChange = ({ target }) => {
         const searchParams = new URLSearchParams(location.search);
 
         if (target !== undefined) {
-            searchParams.set("szukaj", target.value);
+            searchParams.set(searchQueryParamName, target.value);
         } else {
-            searchParams.delete("szukaj");
+            searchParams.delete(searchQueryParamName);
         }
 
         history.push(`${location.pathname}?${searchParams.toString()}`);
