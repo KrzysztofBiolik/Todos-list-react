443d32ab61e10f9d0eac0b3ba85d980d82f40b31
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-10-07 21:06:02 +0200

    Convert Input to TS

--- src/features/tasks/TasksPage/Input/index.ts
+++ src/features/tasks/TasksPage/Input/index.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export default styled.input`
     flex-grow: 1;
-    border: 1px solid ${({ theme }) => theme.color.lightgrey};
+    border: 1px solid ${({ theme }) => theme.color.lightGrey};
     margin: 10px;
     padding: 10px;
 `;
